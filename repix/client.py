a1f4483d60f964beb53249da0fea7fd1d7bd16c6
Mats Lindh <mats@lindh.no>
2017-01-20 15:17:45 +0100

    Override image_url method to generate repix specific image urls

--- repix/client.py
+++ repix/client.py
@@ -1,6 +1,28 @@
 import imboclient
+from repix.url import (
+    ImageURL,
+    CompactAccessTokenGenerator,
+)
 
 
 class Client(imboclient.Client):
-    def __init__(self, server_urls, user=None, public_key=None, private_key=None):
-        pass
+    def __init__(self, server_urls,
+                 user=None,
+                 public_key=None,
+                 private_key=None,
+                 access_token_generator=CompactAccessTokenGenerator):
+        super(Client, self).__init__(server_urls=server_urls,
+                                     user=user,
+                                     public_key=public_key,
+                                     private_key=private_key,
+                                     access_token_generator=access_token_generator)
+
+    def image_url(self, image_identifier):
+        return ImageURL(self._pick_url(image_identifier),
+                        user=self._user,
+                        public_key=self._public_key,
+                        private_key=self._private_key,
+                        image_identifier=image_identifier,
+                        access_token_generator=self.access_token_generator)
+
+
