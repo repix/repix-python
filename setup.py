e3962e2fba42ed5187f24d158fd8d9fd7cd51f03
Mats Lindh <mats@lindh.no>
2017-01-20 15:16:11 +0100

    Set initial version number for setup.py compatibility

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='repix',
-    version='0.0.1',
+    version='0.1.0',
     author='repix.io',
     author_email='dev@repix.io',
     packages=['repix'],
@@ -13,6 +13,6 @@ setup(
     license='LICENSE',
     description='Python client for repix.io',
     long_description=open('README.rst').read(),
-    install_requires=['imboclient==0.1.2', 'nose', ]
+    install_requires=['imboclient==0.1.2', 'pytest', ]
 )
 
